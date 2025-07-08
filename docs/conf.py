f81a4faad98a72d7533f19b2319096072953c342
Walder, Adam Randall <walder2@llnl.gov>
2025-07-08 14:32:19 -0700

    updates for readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -2,10 +2,7 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
-import os
-import sys
-#sys.path.insert(0, os.path.abspath('/Users/walder2/_fake_pyproject'))
-sys.path.insert(0, os.path.abspath('/Users/walder2/pysp_1.0.0/pysparkplug'))
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
