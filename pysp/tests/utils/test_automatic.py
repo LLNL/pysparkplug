d0b1caf9a16986e1bb8f2eb966c4629e38759930
Walder, Adam Randall <walder2@llnl.gov>
2025-07-08 10:34:47 -0700

    updated data paths

--- pysp/tests/utils/test_automatic.py
+++ pysp/tests/utils/test_automatic.py
@@ -7,16 +7,18 @@ import numpy as np
 from pysp.utils.automatic import get_dpm_mixture, get_estimator
 from pysp.bstats import *
 
+DATA_DIR = "pysp/tests/data"
+ANSWER_DIR = "pysp/tests/answerkeys"
 
 @pytest.mark.parametrize("case_id", [0, 1])
 def test_get_dpm_mixture(case_id: int) -> None:
     """Tests if pipeline for creating estimator and estiamting a DPM works."""
-    with open(f"pysp/tests/data/testInput_automatic{case_id}.pkl", 'rb') as f:
+    with open(os.path.join(DATA_DIR, f"testInput_automatic{case_id}.pkl"), 'rb') as f:
         data = pickle.load(f)
     
     model = get_dpm_mixture(data, rng=np.random.RandomState(1))
 
-    with open(f"pysp/tests/answerkeys/testOutput_automatic{case_id}.txt", 'r') as f:
+    with open(os.path.join(ANSWER_DIR, f"testOutput_automatic{case_id}.txt"), 'r') as f:
         answer = f.read()
     
     assert answer == str(model)
