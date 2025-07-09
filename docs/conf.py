175d8a3b96d797a141c5f324018c612e06de4b40
Walder, Adam Randall <walder2@llnl.gov>
2025-07-08 17:56:01 -0700

    added absolute path to conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -2,7 +2,9 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
-
+import sys
+import os
+sys.path.insert(0, os.path.abspath('..'))
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
@@ -21,6 +23,7 @@ extensions = [
     'sphinx.ext.mathjax',
 ]
 
+templates_path = ['_templates']
 exclude_patterns = []
 
 
